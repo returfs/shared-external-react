7e269b03d32a9718eb31ed3934ba2457ef843829
badasukerubin <kelvinvip8@gmail.com>
2024-10-11 11:32:54 +0100

    initial commit

---
+++ Styles/Colors/Background/Background.ts
@@ -0,0 +1,109 @@
+import { Color } from '../types';
+
+export const neutralFiftyEightHundredBgColors =
+  'bg-neutral-50 dark:bg-neutral-800';
+export const neutralNineHundredBgColors = 'dark:bg-neutral-900';
+
+export const whiteEightHundredBgColors: Color = {
+  red: 'bg-white dark:bg-red-800',
+  orange: 'bg-white dark:bg-orange-800',
+  yellow: 'bg-white dark:bg-yellow-800',
+  gray: 'bg-white dark:bg-gray-800',
+  green: 'bg-white dark:bg-green-800',
+  blue: 'bg-white dark:bg-blue-800',
+  indigo: 'bg-white dark:bg-indigo-800',
+  violet: 'bg-white dark:bg-violet-800',
+  purple: 'bg-white dark:bg-purple-800',
+  pink: 'bg-white dark:bg-pink-800',
+};
+
+export const oneHundredEightHundredBgColors: Color = {
+  red: 'bg-red-100 dark:bg-red-800',
+  orange: 'bg-orange-100 dark:bg-orange-800',
+  yellow: 'bg-yellow-100 dark:bg-yellow-800',
+  gray: 'bg-gray-100 dark:bg-gray-800',
+  green: 'bg-green-100 dark:bg-green-800',
+  blue: 'bg-blue-100 dark:bg-blue-800',
+  indigo: 'bg-indigo-100 dark:bg-indigo-800',
+  violet: 'bg-violet-100 dark:bg-violet-800',
+  purple: 'bg-purple-100 dark:bg-purple-800',
+  pink: 'bg-pink-100 dark:bg-pink-800',
+};
+
+export const oneHundredEightHundredBgChildrenColors: Color = {
+  red: '*:bg-red-100 *:dark:bg-red-800',
+  orange: '*:bg-orange-100 *:dark:bg-orange-800',
+  yellow: '*:bg-yellow-100 *:dark:bg-yellow-800',
+  gray: '*:bg-gray-100 *:dark:bg-gray-800',
+  green: '*:bg-green-100 *:dark:bg-green-800',
+  blue: '*:bg-blue-100 *:dark:bg-blue-800',
+  indigo: '*:bg-indigo-100 *:dark:bg-indigo-800',
+  violet: '*:bg-violet-100 *:dark:bg-violet-800',
+  purple: '*:bg-purple-100 *:dark:bg-purple-800',
+  pink: '*:bg-pink-100 *:dark:bg-pink-800',
+};
+
+export const twoHundredNineHundredBgColors: Color = {
+  red: 'bg-red-200 dark:bg-red-900',
+  orange: 'bg-orange-200 dark:bg-orange-900',
+  yellow: 'bg-yellow-200 dark:bg-yellow-900',
+  gray: 'bg-gray-200 dark:bg-gray-900',
+  green: 'bg-green-200 dark:bg-green-900',
+  blue: 'bg-blue-200 dark:bg-blue-900',
+  indigo: 'bg-indigo-200 dark:bg-indigo-900',
+  violet: 'bg-violet-200 dark:bg-violet-900',
+  purple: 'bg-purple-200 dark:bg-purple-900',
+  pink: 'bg-pink-200 dark:bg-pink-900',
+};
+
+export const eightHundredOneHundredBgColors: Color = {
+  red: 'bg-red-800 dark:bg-red-100',
+  orange: 'bg-orange-800 dark:bg-orange-100',
+  yellow: 'bg-yellow-800 dark:bg-yellow-100',
+  gray: 'bg-gray-800 dark:bg-gray-100',
+  green: 'bg-green-800 dark:bg-green-100',
+  blue: 'bg-blue-800 dark:bg-blue-100',
+  indigo: 'bg-indigo-800 dark:bg-indigo-100',
+  violet: 'bg-violet-800 dark:bg-violet-100',
+  purple: 'bg-purple-800 dark:bg-purple-100',
+  pink: 'bg-pink-800 dark:bg-pink-100',
+};
+
+export const nineFiftyTwoHundredBgColors: Color = {
+  red: 'bg-red-950 dark:bg-red-200',
+  orange: 'bg-orange-950 dark:bg-orange-200',
+  yellow: 'bg-yellow-950 dark:bg-yellow-200',
+  gray: 'bg-gray-950 dark:bg-gray-200',
+  green: 'bg-green-950 dark:bg-green-200',
+  blue: 'bg-blue-950 dark:bg-blue-200',
+  indigo: 'bg-indigo-950 dark:bg-indigo-200',
+  violet: 'bg-violet-950 dark:bg-violet-200',
+  purple: 'bg-purple-950 dark:bg-purple-200',
+  pink: 'bg-pink-950 dark:bg-pink-200',
+};
+
+export const fiftyNineFiftyBgColors: Color = {
+  red: 'bg-red-50 dark:bg-red-950',
+  orange: 'bg-orange-50 dark:bg-orange-950',
+  yellow: 'bg-yellow-50 dark:bg-yellow-950',
+  gray: 'bg-gray-50 dark:bg-gray-950',
+  green: 'bg-green-50 dark:bg-green-950',
+  blue: 'bg-blue-50 dark:bg-blue-950',
+  indigo: 'bg-indigo-50 dark:bg-indigo-950',
+  violet: 'bg-violet-50 dark:bg-violet-950',
+  purple: 'bg-purple-50 dark:bg-purple-950',
+  pink: 'bg-pink-50 dark:bg-pink-950',
+};
+
+export const hoverOneHundredEightHundredBgColors: Color = {
+  red: 'hover:bg-red-100 dark:hover:bg-red-800',
+  orange: 'hover:bg-orange-100 dark:hover:bg-orange-800',
+  yellow: 'hover:bg-yellow-100 dark:hover:bg-yellow-800',
+  gray: 'hover:bg-gray-100 dark:hover:bg-gray-800',
+  green: 'hover:bg-green-100 dark:hover:bg-green-800',
+  blue: 'hover:bg-blue-100 dark:hover:bg-blue-800',
+  indigo: 'hover:bg-indigo-100 dark:hover:bg-indigo-800',
+  violet: 'hover:bg-violet-100 dark:hover:bg-violet-800',
+  purple: 'hover:bg-purple-100 dark:hover:bg-purple-800',
+  pink: 'hover:bg-pink-100 dark:hover:bg-pink-800',
+};
