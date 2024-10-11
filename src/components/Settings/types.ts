358d95b962042c1f74f377e728f8e3f891d76db5
badasukerubin <kelvinvip8@gmail.com>
2024-10-11 15:01:13 +0100

    feat: added text colors

---
+++ src/components/Settings/types.ts
@@ -0,0 +1,10 @@
+export enum ThemeData {
+  ThemeVariant = 'theme-variant',
+  ThemeColor = 'theme-color',
+}
+
+export enum ThemeVariant {
+  Dark = 'dark',
+  Light = 'light',
+  SystemDefault = 'system-default',
+}
---
+++ src/styles/colors/Text/Text.ts
@@ -0,0 +1,66 @@
+import { Color } from '../types';
+
+export const twoHundredNineFiftyTextColors: Color = {
+  red: 'text-red-200 dark:text-red-950',
+  orange: 'text-orange-200 dark:text-orange-950',
+  yellow: 'text-yellow-200 dark:text-yellow-950',
+  gray: 'text-gray-200 dark:text-gray-950',
+  green: 'text-green-200 dark:text-green-950',
+  blue: 'text-blue-200 dark:text-blue-950',
+  indigo: 'text-indigo-200 dark:text-indigo-950',
+  violet: 'text-violet-200 dark:text-violet-950',
+  purple: 'text-purple-200 dark:text-purple-950',
+  pink: 'text-pink-200 dark:text-pink-950',
+};
+
+export const sixHundredThreeHundredTextColors: Color = {
+  red: 'text-red-600 dark:text-red-300',
+  orange: 'text-orange-600 dark:text-orange-300',
+  yellow: 'text-yellow-600 dark:text-yellow-300',
+  gray: 'text-gray-600 dark:text-gray-300',
+  green: 'text-green-600 dark:text-green-300',
+  blue: 'text-blue-600 dark:text-blue-300',
+  indigo: 'text-indigo-600 dark:text-indigo-300',
+  violet: 'text-violet-600 dark:text-violet-300',
+  purple: 'text-purple-600 dark:text-purple-300',
+  pink: 'text-pink-600 dark:text-pink-300',
+};
+
+export const sixHundredFourHundredTextColors: Color = {
+  red: 'text-red-600 dark:text-red-400',
+  orange: 'text-orange-600 dark:text-orange-400',
+  yellow: 'text-yellow-600 dark:text-yellow-400',
+  gray: 'text-gray-600 dark:text-gray-400',
+  green: 'text-green-600 dark:text-green-400',
+  blue: 'text-blue-600 dark:text-blue-400',
+  indigo: 'text-indigo-600 dark:text-indigo-400',
+  violet: 'text-violet-600 dark:text-violet-400',
+  purple: 'text-purple-600 dark:text-purple-400',
+  pink: 'text-pink-600 dark:text-pink-400',
+};
+
+export const nineHundredOneHundredTextColors: Color = {
+  red: 'text-red-900 dark:text-red-100',
+  orange: 'text-orange-900 dark:text-orange-100',
+  yellow: 'text-yellow-900 dark:text-yellow-100',
+  gray: 'text-gray-900 dark:text-gray-100',
+  green: 'text-green-900 dark:text-green-100',
+  blue: 'text-blue-900 dark:text-blue-100',
+  indigo: 'text-indigo-900 dark:text-indigo-100',
+  violet: 'text-violet-900 dark:text-violet-100',
+  purple: 'text-purple-900 dark:text-purple-100',
+  pink: 'text-pink-900 dark:text-pink-100',
+};
+
+export const nineFiftyTwoHundredTextColors: Color = {
+  red: 'text-red-950 dark:text-red-200',
+  orange: 'text-orange-950 dark:text-orange-200 ',
+  yellow: 'text-yellow-950 dark:text-yellow-200 ',
+  gray: 'text-gray-950 dark:text-gray-200',
+  green: 'text-green-950 dark:text-green-200 ',
+  blue: 'text-blue-950 dark:text-blue-200 ',
+  indigo: 'text-indigo-950 dark:text-indigo-200 ',
+  violet: 'text-violet-950 dark:text-violet-200 ',
+  purple: 'text-purple-950 dark:text-purple-200 ',
+  pink: 'text-pink-950 dark:text-pink-200 ',
+};
