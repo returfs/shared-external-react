794bbdba0d0d599074af02989151f03e66846168
badasukerubin <kelvinvip8@gmail.com>
2024-11-30 22:20:03 +0100

    feat: redefining packages

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,6 +3,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import terser from '@rollup/plugin-terser';
+import postcss from 'rollup-plugin-postcss';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 import packageJson from './package.json' assert { type: 'json' };
@@ -26,6 +27,10 @@ export default [
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
       terser(),
+      postcss({
+        extract: true,
+        minimize: true,
+      }),
       //   visualizer({
       //     filename: './bundle-analysis.html',
       //     open: true,
@@ -43,5 +48,16 @@ export default [
     input: 'dist/esm/types/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
+    external: [/\.css$/],
+  },
+  {
+    input: 'src/index.css',
+    output: [{ file: 'dist/index.css', format: 'es' }],
+    plugins: [
+      postcss({
+        extract: true,
+        minimize: true,
+      }),
+    ],
   },
 ];
